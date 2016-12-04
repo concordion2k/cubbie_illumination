9f3db4f631fd7adc4cdcb14a141ea8b4a319f7b2
Dan Douston <concordion@mac.com>
2016-12-04 15:01:41 -0500

    Committing a more precise sine-wave function that returns 0 and 1 properly. Changing the curve in the future will require ensuring (0,0) starts at 0 and the function hits 1 at some point. In short: yay derivatives!

--- illumination/illum_curve.py
+++ illumination/illum_curve.py
@@ -5,13 +5,13 @@ pi = math.pi
 counter = 0.0
 bright = 0.0
 increment = 0.1
-amplitude = -0.5
-wavelength = 2
+amplitude = 0.5
+wavelength = -(pi/2)
 xTransform = 0.8
 yTransform = .5
 
 # Illuminate LED...
-while bright < .98:
+while bright < 1:
         counter = counter + increment
         bright = (amplitude * math.sin(wavelength * (counter + xTransform))) + yTransform
         print "b: " + format(bright, '.4f') + " | " + "c: " + format(counter, '.2f') + " | ",

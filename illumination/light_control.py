c7212c6ff88f073ebb181c14ddcf43287dc1b1a3
Dan Douston <concordion@mac.com>
2016-12-05 15:34:09 -0500

    adding a sequential lightning setup in light_control.py

--- illumination/light_control.py
+++ illumination/light_control.py
@@ -1,7 +1,23 @@
 from illuminate import *
 
-illum = illumination()
-print "Class assigned"
+c1 = illumination()
+c2 = illumination()
+c3 = illumination()
+c4 = illumination()
 
-illum.turnOn()
-illum.turnOff()
+print "Classes assigned"
+
+c1.turnOn()
+
+#time.sleep(0.5)
+
+c2.turnOn()
+
+#time.sleep(0.5)
+
+c3.turnOn()
+
+#time.sleep(0.5)
+
+c4.turnOn()
+
